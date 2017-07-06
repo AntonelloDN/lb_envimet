a5f48394671ff41dcc08fdc9e18e46be22c35464
AntonelloDN <antonellodinunzio@gmail.com>
2017-07-07 00:41:10 +0200

    Add files via upload

---
+++ lb_envimet/__init__.py
@@ -0,0 +1,30 @@
+# LB ENVI-Met Core
+#
+# lb_envimet: A series of components to connect Ladybug and Gismo to Envimet for urban microclimatic analysis
+#
+# This file is part of lb_envimet.
+#
+# Copyright (c) 2013-2017, Antonello Di Nunzio <antonellodinunzio@gmail.com>
+# Ladybug is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published
+# by the Free Software Foundation; either version 3 of the License,
+# or (at your option) any later version.
+#
+# LB ENVI-Met Core is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ladybug; If not, see <http://www.gnu.org/licenses/>.
+#
+# @license GPL-3.0+ <http://spdx.org/licenses/GPL-3.0+>
+
+
+__author__ = "Antonello Di Nunzio <antonellodinunzio@gmail.com>"
+__date__ = "24 Jan 2017"
+__version__ = '0.0.1'
+
+
+from . import envimetStuff
+from . import envimetObj
